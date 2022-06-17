c1d25c50147ad626903f0173b04de54b73cf8b18
Navid Kianfar <nvd.kianfar@gmail.com>
2022-06-17 22:01:19 +0300

    login prepared #2

--- src/app/pages/account/login/login.component.ts
+++ src/app/pages/account/login/login.component.ts
@@ -3,6 +3,8 @@ import {IdentityService} from "../../../services/account/identity.service";
 import {OperationResultStatus} from "../../../lib/operation-result";
 import {Router} from "@angular/router";
 import {NotificationService} from "../../../services/core/notification.service";
+import {StringHelpers} from "../../../lib/helpers/string.helpers";
+import {ValidationHelpers} from "../../../lib/helpers/validation.helpers";
 
 @Component({
   selector: 'app-login',
@@ -35,11 +37,29 @@ export class LoginComponent implements OnInit {
   }
 
   validatePassword(): boolean {
-    return true;
+    this.validations.password = '';
+    const password = this.forms.password.trim();
+
+    if (!password) {
+      this.validations.password = 'PASSWORD_REQUIRED';
+    } else if (password.length < 6) {
+      this.validations.password = 'PASSWORD_MIN_LENGTH';
+    }
+
+    return this.validations.password === '';
   }
 
   validateEmail(): boolean {
-    return true;
+    this.validations.email = '';
+    const email = this.forms.email.trim();
+
+    if (!email) {
+      this.validations.email = 'EMAIL_REQUIRED';
+    } else if (!ValidationHelpers.isEmail(email)) {
+      this.validations.email = 'EMAIL_INVALID';
+    }
+
+    return this.validations.email === '';
   }
 
   togglePasswordVisibility($event: MouseEvent, element: HTMLInputElement) {
